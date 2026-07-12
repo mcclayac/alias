65a239b0e948729346ac8bf5aefa2399e73ca3ba
anthonymcclay <anthony.mcclay@gmail.com>
2026-07-12 09:07:01 -0500

    more apps and services

--- kubernetes/yaml/kubernetesForBeginers/services/testCurl3000.sh
+++ kubernetes/yaml/kubernetesForBeginers/services/testCurl3000.sh
@@ -4,7 +4,15 @@ for loopvar in {1..200}
 do
         echo loop : $loopvar
 	curl http://127.0.0.1:3000/hostname
+	echo "  "
+	curl http://127.0.0.1:4000/hostname
+	echo "  "
+	curl http://127.0.0.1:6000/hostname
 	#curl http://127.0.0.1:55598/hostname
+        #curl http://127.0.0.1:55071/hostname
+        #curl http://127.0.0.1:55537/hostname
+	echo "  "
+	echo "  "
 	echo "  "
 done
 
